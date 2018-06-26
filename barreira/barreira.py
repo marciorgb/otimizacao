fc8649273175d70fc0d12643f21fc51b0d8b07e2
Márcio Eduardo Gomes Barbosa (MarcioRGB) <marcio@aptans.com>
2018-06-26 20:59:40 -0300

    Adiciona testes de barreira

--- barreira/barreira.py
+++ barreira/barreira.py
@@ -1,16 +1,20 @@
+from sympy import *
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Compute the x and y coordinates for points on a sine curve
-x = np.arange(0, 3 * np.pi, 0.5)
-y = np.sin(x)
+x = Symbol('x')
 
-a = np.arange(0, 3 * np.pi, 0.001)
-b = np.cos(a)
+y = x**2 + x*2
 
-plt.subplot(2, 1, 1)
+y_derivated = y.diff(x)
+y_derivated
+
+a = np.arange(0, 3, 0.001)
+b = y_derivated
+
 # Plot the points using matplotlib
-plt.plot(x, y)
-plt.subplot(2, 1, 2)
+plt.subplot(2, 1, 1)
 plt.plot(a, b)
 plt.show()  # You must call plt.show() to make graphics appear.

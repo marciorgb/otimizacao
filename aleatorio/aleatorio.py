3b1a4b7a9d021423f6babf834363c30dd04f1692
Márcio Eduardo Gomes Barbosa (MarcioRGB) <marcio@aptans.com>
2018-06-28 15:49:16 -0300

    Corrige função de aleatoriedade

--- aleatorio/aleatorio.py
+++ aleatorio/aleatorio.py
@@ -1,34 +1,57 @@
-from random import random
+import sympy
+import random
+import matplotlib.pyplot as plt
+from sympy.abc import x, y, alpha
 
-# function to optimize: takes in a list of decision variables, returns an objective value
-# this is the Rosenbrock function: http://en.wikipedia.org/wiki/Rosenbrock_function
-# the global minimum is located at x = (1,1) where f(x) = 0
-def my_function(x):
-  return (1-x[0])**2 + 100*(x[1] - x[0]**2)**2
+#inicaliza as variáveis
+alcance = [-1 , 1]
+dk = [0, 0]
+X = [ 0, 0]
+function = x**2 +y**2
+ini = [-3, 2]
+valor_no_ponto = [3, 2]
+plotX = []
+plotY = []
 
-# function to perform (a very crude, stupid) optimization
-# bounds = lower and upper bounds for each decision variable (2D list)
-# NFE = number of function evaluations to perform
-# f = the function to be optimized
-def optimize(bounds, NFE, f):
-  D = len(bounds) # number of decision variables
-  best_f = 9999.0 # initialize the "best found" - both the function value and the x values
-  best_x = [None]*D
+def optimize(a, b):
+    i = 0
+    while ( i <10 ):
+        # printa a iteração
+        print("+++++++++ ITERAÇÃO {} +++++++++".format(i))
+        # inicializa as variáveis aleatŕoiamente 
+        dk[0] = random.uniform(alcance[0], alcance[1])
+        dk[1] = random.uniform(alcance[0], alcance[1])
+        print(dk)
+        # as aplica na função objetivo
+        # aplica xk + aplhak *  dk
+        X[0] = ini[0] + alpha * dk[0]
+        X[1] = ini[1] + alpha * dk[1]
+        # aplica X na função f
+        alpha_k = function.subs( x, X[0])
+        alpha_k = alpha_k.subs(y, X[1])
+        # deriva a função alpha_K achando seu ponto de máximo
+        alpha_k = alpha_k.diff( alpha )
+        # resolver alpha
+        solucao = sympy.solve( alpha_k, alpha)
+        #import pdb; pdb.set_trace()
+        ini[0] = X[0].subs(alpha, solucao[0])
+        ini[1] = X[1].subs(alpha, solucao[0])
+        final = function.subs( x, ini[0])
+        final = final.subs(y, ini[1])
+        # printa as soluções no terminal
+        print("solução de iteração {} é {} com os pontos: ({}, {})".format(i, solucao, ini[0], ini[1]))
+        # itera o i
+        plotX.append(ini[0])
+        plotY.append(ini[1])
+        i += 1
+    plt.plot(plotX, plotY, 'ro')
+    plt.show()
 
-  for i in range(NFE):
-    # use an "operator" to generate a new candidate solution
-    # this is "uniform mutation" in MOEA lingo
-    new_x = [bounds[d][0] + random()*(bounds[d][1] - bounds[d][0]) for d in range(D)]
-    new_f = f(new_x)
-    if new_f < best_f: # see if it's an improvement -- in multiobjective, this is the Pareto sort
-      best_f = new_f
-      best_x = new_x
-
-  return {'best_x': best_x, 'best_f': best_f}
-
-
-# now let's try it...
-# (the Rosenbrock problem technically doesn't have "bounds", but we'll make some up..)
-bounds = [[-1,5], [-1,5]]
-result = optimize(bounds, 10000, my_function)
-print result # it should be near best_f = 0.0 and best_x = [1,1], hopefully
+if __name__ == "__main__":
+    #alcance[0] = input("Insira o range de aletoriedade [-1,1](um valor por vez):")
+    #alcance[1] = input()
+    #function = sympy.simplify(input("Insira a equação objetivo [x**2 + y**2]"))
+    #ini[0] = input("Insira o valor do ponto [ -3, 2]")
+    #ini[1] = input()
+    print("Iremos Otimizar a função F(x) = {}".format(function))
+    optimize(valor_no_ponto[0], valor_no_ponto[1])

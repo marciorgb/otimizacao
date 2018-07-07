a19e37f0a31ef1ba2b9c7a2805b117c6262668a0
Márcio Eduardo Gomes Barbosa (MarcioRGB) <marcio@aptans.com>
2018-07-07 09:52:28 -0300

    Altera algoritmo otimização aleatória

--- aleatorio/aleatorio.py
+++ aleatorio/aleatorio.py
@@ -2,20 +2,38 @@ import sympy
 import random
 import matplotlib.pyplot as plt
 from sympy.abc import x, y, alpha
+import numpy as np
 
 #inicaliza as variáveis
-alcance = [-1 , 1]
-dk = [0, 0]
-X = [ 0, 0]
-function = x**2 +y**2
-ini = [-3, 2]
-valor_no_ponto = [3, 2]
-plotX = []
-plotY = []
+alcance = [-1 , 1] # range do número aleatório
+dk = [0, 0] # incialização da lista 
+X = [ 0, 0] # X = x0 + alpha * dk
+function = x**2 + y**2 -300 # a função em sí
+ini = [50, 100] # ponto inicial
+valor_no_ponto = [3, 2] 
+plotX = [] # eixo X
+plotY = [] # eixo Y
+vetor = [] # vetor de Comparação
+
+
+def Criterio(vetor,valor):
+
+    valores = [0,0,0,0,0]
+    vetor.append(valor)
+ 
+    if(len(vetor)>=5):
+        delta= max(vetor)-min(vetor)
+        for i in range(5): # copia os últimos 5 elementos para um auxiliar 
+            valores[i] = vetor[-(i+1)]
+        dround = max(valores)-min(valores)
+        if(dround <= 0.000001*delta):
+            return 1
+    return 0
 
 def optimize(a, b):
     i = 0
-    while ( i <10 ):
+    solucao = 1
+    while (i < 100 ):
         # printa a iteração
         print("+++++++++ ITERAÇÃO {} +++++++++".format(i))
         # inicializa as variáveis aleatŕoiamente 
@@ -36,22 +54,25 @@ def optimize(a, b):
         #import pdb; pdb.set_trace()
         ini[0] = X[0].subs(alpha, solucao[0])
         ini[1] = X[1].subs(alpha, solucao[0])
+        # gera o valor final de F aplicado nos pontos
         final = function.subs( x, ini[0])
         final = final.subs(y, ini[1])
         # printa as soluções no terminal
         print("solução de iteração {} é {} com os pontos: ({}, {})".format(i, solucao, ini[0], ini[1]))
-        # itera o i
-        plotX.append(ini[0])
-        plotY.append(ini[1])
+        # adiciona para plotagem
+        plotX.append(final)
+        print("----------------- Valor Final") 
+        print(final)	
+        if(Criterio( vetor, final )==1):
+            break
+
         i += 1
-    plt.plot(plotX, plotY, 'ro')
+    
+    
+
+    plt.plot(plotX, 'ro')
     plt.show()
 
 if __name__ == "__main__":
-    #alcance[0] = input("Insira o range de aletoriedade [-1,1](um valor por vez):")
-    #alcance[1] = input()
-    #function = sympy.simplify(input("Insira a equação objetivo [x**2 + y**2]"))
-    #ini[0] = input("Insira o valor do ponto [ -3, 2]")
-    #ini[1] = input()
     print("Iremos Otimizar a função F(x) = {}".format(function))
     optimize(valor_no_ponto[0], valor_no_ponto[1])
